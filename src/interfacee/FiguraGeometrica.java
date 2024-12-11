ba1a6330f6ac433603e351bbdee4192fd4ada514
Juan Pablo <rovayojuanpablo@gmail.com>
2024-12-11 16:54:28 -0500

    Update: Rectangle class added

---
+++ src/interfacee/FiguraGeometrica.java
@@ -0,0 +1,7 @@
+package interfacee;
+
+public interface FiguraGeometrica {
+	
+	public double calcularArea();
+	
+}
--- src/interfacee/Main.java
+++ src/interfacee/Main.java
@@ -4,6 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Pablo");
+		
+		Rectangulo rectangulo1 = new Rectangulo();
+		
+		rectangulo1.setAltura(15);
+		rectangulo1.setBase(30);
+		
+		System.out.println("Área del rectángulo " + rectangulo1.calcularArea());
 
 	}
 
